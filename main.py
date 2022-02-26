744edd61508c0de0844784e4a5833455aba787ac
陆乐 <lule13913866396@163.com>
2022-02-26 13:28:30 +0800

    加入加载进度条

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import progressbar
+bar = progressbar.ProgressBar(max_value=100)
+print("加载中……")
+
 import logging as log
 import sys
 
 from PyQt5.QtWidgets import QApplication
+bar.update(5)
 
 from myOperations import *
+bar.update(10)
 from myWindows import myMain
+bar.update(15)
+
 
 
 def setUp():
@@ -15,11 +23,16 @@ def setUp():
 
 def main():
     setUp()
+    bar.update(20)
 
     try:
         app = QApplication(sys.argv)
+        bar.update(50)
         mainWid = myMain()
+        bar.update(70)
         mainWid.show()
+        bar.update(100)
+        print("加载完成")
         sys.exit(app.exec_())
     except Exception as e:
         log.fatal("\n\tFatal Error: %s\n\tProgram exited."%str(e))
